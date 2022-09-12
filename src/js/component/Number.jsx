3af1bf7175da9937c25416073c92948d875bee6f
sergiodsg <sergiodasilva.dev@gmail.com>
2022-09-12 19:16:31 +0000

    Añadidos estilos básicos

--- src/js/component/Number.jsx
+++ src/js/component/Number.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 const Number = props => {
-    return(<span>{props.seconds}</span>);
+    return(<div className="col-1 text-center fs-2 text-light">{props.seconds}</div>);
 }
 
 Number.propTypes = {
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Number from "./Number.jsx";
 import PropTypes from 'prop-types';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faFontAwesome } from "@fortawesome/free-solid-svg-icons";
+// import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,7 +12,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 	console.log(props.digits);
 	return (
-		<div>
+		<div className="row justify-content-center bg-dark mx-2">
+			{/* <FontAwesomeIcon icon="fa-solid fa-clock" /> */}
 			<Number seconds={Math.floor((props.digits/100000))%10}/>
 			<Number seconds={Math.floor((props.digits/10000))%10}/>
 			<Number seconds={Math.floor((props.digits/1000))%10}/>
