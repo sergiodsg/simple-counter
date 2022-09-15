6e593c012e41ca1cd9409c2ab9504636547b5b93
sergiodsg <sergiodasilva.dev@gmail.com>
2022-09-15 20:28:29 +0000

    Añadido ícono de reloj en la izquierda

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Number from "./Number.jsx";
 import PropTypes from 'prop-types';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faFontAwesome } from "@fortawesome/free-solid-svg-icons";
-// import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -12,8 +11,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 	console.log(props.digits);
 	return (
-		<div className="row justify-content-center bg-dark mx-2">
-			{/* <FontAwesomeIcon icon="fa-solid fa-clock" /> */}
+		<div className="row justify-content-center bg-dark py-3">
+			<div className="col-1">
+				<FontAwesomeIcon className="text-light fs-1 m-auto" icon={ faClock } />
+			</div>
 			<Number seconds={Math.floor((props.digits/100000))%10}/>
 			<Number seconds={Math.floor((props.digits/10000))%10}/>
 			<Number seconds={Math.floor((props.digits/1000))%10}/>
